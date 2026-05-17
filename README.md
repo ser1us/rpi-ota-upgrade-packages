8832fb9ac72e366c1de824cd485370d2a9c38fe0
Christoph Lipp <christoph@lipp.ws>
2026-05-17 13:10:18 +0200

    more changes

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Url:
 `https://raw.githubusercontent.com/ser1us/rpi-ota-upgrade-packages/refs/heads/main/full-upgrade/full-upgrade.tar.zst`
 
 Sha257:
-`e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855`
+`e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855`
--- full-upgrade/full-upgrade.sh
+++ full-upgrade/full-upgrade.sh
@@ -2,6 +2,19 @@
 # Unattended full system upgrade
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get full-upgrade -y --allow-downgrades --purge --auto-remove
-apt-get autoremove -y
-apt-get clean
+apt update
+if apt -y -o DPKG::Options::="--force-confnew --allow-downgrades --purge --auto-remove" full-upgrade >output.txt; then
+  apt-get autoremove -y
+  apt-get clean
+  if [ -r /var/run/reboot-required ]; then
+    echo Rebooting to finish the upgrade
+    exit 2 # EXIT_REBOOT
+  fi
+else
+  echo Upgrade failed:
+  echo
+  cat output.txt
+  exit 1 # EXIT_FAILURE
+fi
+echo Upgrade complete
+exit 0 # EXIT_SUCCESS
